fe8bb4ce7789d1c596bdee2168a4a23b3cc9e461
Ravi Teja <rbailapu@qti.qualcomm.com>
2026-06-03 16:22:26 +0530

    ssifbridged: drop late D-Bus response after timeout
    Set timedOut flag when a timeout response is sent to the host.
    Check the flag in afterMethodCall() to drop any late D-Bus response,
    preventing a duplicate response for the same request.
    
    Change-Id: Icf738feb5de56b39eb3d178ce1880694248de0f4
    Signed-off-by: Ravi Teja <rbailapu@qti.qualcomm.com>

--- ssifbridged.cpp
+++ ssifbridged.cpp
@@ -102,6 +102,11 @@ class SsifChannel
     int showNumOfReqNotRsp() const;
     boost::asio::posix::stream_descriptor dev;
     IpmiCmd prevReqCmd{};
+    /* Set when a timeout response has already been sent for the current
+     * request; cleared on each new incoming request.  Ensures any late
+     * D-Bus response is dropped so the host never receives two responses
+     * for one request. */
+    bool timedOut = false;
 
   protected:
     bool handleGetSysIfCap(uint8_t netfn, uint8_t cmd,
@@ -247,6 +252,8 @@ void rspTimerHandler(const boost::system::error_code& ec)
     rsp[ssifchannel->sizeofLenField + 1] = prevReqCmd.cmd;
     rsp[ssifchannel->sizeofLenField + 2] = ccResponseNotAvailable;
 
+    ssifchannel->timedOut = true;
+
     boost::system::error_code ecWr;
 
     size_t wlen =
@@ -288,7 +295,8 @@ void SsifChannel::afterMethodCall(const boost::system::error_code& ec,
         return;
     }
 
-    if ((prevReqCmd.netfn != (netfn - 1) || prevReqCmd.lun != lun ||
+    if (timedOut ||
+        (prevReqCmd.netfn != (netfn - 1) || prevReqCmd.lun != lun ||
          prevReqCmd.cmd != cmd) ||
         ((prevReqCmd.netfn == (netfn - 1) && prevReqCmd.lun == lun &&
           prevReqCmd.cmd == cmd) &&
@@ -382,6 +390,7 @@ void SsifChannel::processMessage(const boost::system::error_code& ecRd,
 
     /* there is a request coming */
     numberOfReqNotRsp++;
+    timedOut = false;
     /* start response timer */
     rspTimer.expires_after(std::chrono::microseconds(hostReqTimeout));
     rspTimer.async_wait(rspTimerHandler);
